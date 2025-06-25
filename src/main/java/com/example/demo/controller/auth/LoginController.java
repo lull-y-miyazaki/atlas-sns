eebb9cbeb07415b2f52d1c483fb9a71a921d5328
lull-y-miyazaki <y.miyazaki@lull-inc.co.jp>
2025-06-25 18:26:44 +0900

    feat: ログイン画面の表示、処理の実装

--- src/main/java/com/example/demo/controller/auth/LoginController.java
+++ src/main/java/com/example/demo/controller/auth/LoginController.java
@@ -1,9 +1,78 @@
 // ログイン処理のController
 package com.example.demo.controller.auth;
 
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.User;
+import com.example.demo.model.Account;
+import com.example.demo.repository.UserRepository;
 
 @Controller
+@RequestMapping("/auth")
 public class LoginController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private HttpSession session;
+
+	@Autowired
+	private Account account;
+
+	// ログイン画面の表示
+	@GetMapping({ "/login", "/logout" })
+	public String showLogin(
+			@RequestParam(defaultValue = "") String error,
+			Model model) {
+
+		// セッション情報全てをクリア
+		session.invalidate();
+
+		// クエリパラメータで"notLoggedIn"を受け取った場合
+		if (error.equals("notLoggedIn")) {
+			model.addAttribute("message", "ログインしてください");
+		}
+
+		return "auth/login";
+	}
+
+	// ログイン処理
+	@PostMapping("/login")
+	public String login(
+			@RequestParam String email,
+			@RequestParam String password,
+			Model model) {
+
+		// email または password が空の場合にエラーメッセージを表示
+		if (email == null || email.isEmpty() || password == null || password.isEmpty()) {
+			model.addAttribute("message", "メールアドレスとパスワードを入力してください");
+			return "auth/login";
+		}
+
+		// データーベースからユーザー情報を取得
+		User user = userRepository.findByEmailAndPassword(email, password);
+
+		// email または password が正しくない場合にエラーメッセージを表示
+		if (user == null) {
+			model.addAttribute("message", "メールアドレスまたはパスワードが正しくありません");
+			return "auth/login";
+		}
+
+		// ログイン情報をAccountに保存
+		account.setId(user.getId());
+		account.setUsername(user.getName());
+		// account.setIcon(user.getIcon());
+
+		return "redirect:/";
+	}
 
 }
