7faf1966856a11f0004b0d0d916a3ddb8718e931
lull-y-miyazaki <y.miyazaki@lull-inc.co.jp>
2025-06-27 13:24:26 +0900

    feat: フォロー、フォロワー一覧ページの表示の実装

---
+++ src/main/java/com/example/demo/controller/user/FollowListController.java
@@ -0,0 +1,57 @@
+package com.example.demo.controller.user;
+
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.entity.Follow;
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+
+@Controller
+@RequestMapping("/users")
+public class FollowListController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	// フォローリスト一覧画面の表示
+	@GetMapping("/{id}/followings")
+	public String showFollowings(
+			@PathVariable Integer id,
+			Model model) {
+
+		User user = userRepository.findById(id).orElseThrow();
+
+		model.addAttribute("userList", user.getFollowings().stream()
+				.map(Follow::getFollowee)
+				.collect(Collectors.toList()));
+
+		model.addAttribute("listType", "followings");
+
+		return "users/followList";
+	}
+
+	// フォロワー一覧画面の表示
+	@GetMapping("/{id}/followers")
+	public String showFollowers(
+			@PathVariable Integer id,
+			Model model) {
+
+		User user = userRepository.findById(id).orElseThrow();
+
+		model.addAttribute("userList", user.getFollowers().stream()
+				.map(Follow::getFollower)
+				.collect(Collectors.toList()));
+
+		model.addAttribute("listType", "followers");
+
+		return "users/followList";
+	}
+
+}
